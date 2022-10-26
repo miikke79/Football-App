074abc22ceb25a74d48529001ea772e65356205b
miikke79 <74132243+miikke79@users.noreply.github.com>
2022-10-26 10:17:47 +0300

    folders created

--- App.js
+++ App.js
@@ -1,10 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, Image, TextInput, Button, View, Alert, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [stats, setStats] = useState([]);
+  const [country, setCountry] = useState('');
+
+  const fetchStats = () => 
+  {  fetch('https://app.sportdataapi.com/api/v1/soccer/teams?apikey=74723a20-4e0d-11ed-8c29-118268eb2cdb&country_id=106')  
+  .then(response => response.json())
+  .then(data => setStats(data.data[1]))
+  .then(console.log(stats.logo))
+  .catch(error => {         Alert.alert('Error', error);   });
+} 
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>football stats</Text>
+      <Image source={{ uri: stats.logo }} style={styles.pictures}></Image>
+      <Button
+        title='Get Stats'
+        onPress={fetchStats}
+        />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +34,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+
+  pictures: {
+    width:190,
+    height:190,
+    borderColor:'#d35647',
+    resizeMode:'contain',
   },
 });
